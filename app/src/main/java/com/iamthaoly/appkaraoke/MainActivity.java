fdac2c76bb83a0a2647a89b57af0b9774c813b93
Ly <ttly.251242@gmail.com>
2020-04-23 16:36:37 +0700

    Commit lan 2

--- app/src/main/java/com/iamthaoly/appkaraoke/MainActivity.java
+++ app/src/main/java/com/iamthaoly/appkaraoke/MainActivity.java
@@ -2,9 +2,16 @@ package com.iamthaoly.appkaraoke;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Adapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+
+import com.iamthaoly.adapter.BaiHatAdapter;
+import com.iamthaoly.model.BaiHat;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -16,16 +23,54 @@ public class MainActivity extends AppCompatActivity {
     public static SQLiteDatabase database = null;
     public static String TableName = "ArirangSongList";
     String DB_PATH_SUFFIX = "/databases/";
+    ListView lvAll;
+    BaiHatAdapter adapterAll;
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         processCopy();
+        setUpTabHost();
         addControls();
+        hienThiToanBo();
+    }
+
+    private void setUpTabHost() {
+        tabHost = findViewById(R.id.tabhost);
+        tabHost.setup();
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1");
+        tab1.setContent(R.id.tab1);
+        tab1.setIndicator("All");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2");
+        tab2.setContent(R.id.tab2);
+        tab2.setIndicator("Favourite");
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+    }
+
+    private void hienThiToanBo() {
+        database = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+        Cursor c = database.query(TableName, null, null, null, null, null, null);
+        adapterAll.clear();
+        while(c.moveToNext())
+        {
+            String ma = c.getString(0);
+            String ten = c.getString(1);
+            String casy = c.getString(3);
+            int thich = c.getInt(5);
+            BaiHat baiHat = new BaiHat(ma, ten, casy, thich);
+            adapterAll.add(baiHat);
+            c.close();
+        }
     }
 
     private void addControls() {
-
+        lvAll = findViewById(R.id.lvAll);
+        adapterAll = new BaiHatAdapter(MainActivity.this, R.layout.item);
+        lvAll.setAdapter(adapterAll);
     }
 
     //copy database
